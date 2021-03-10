32462094e0863ee103c41dd1528f65d00352d31a
abobija <abobijax@gmail.com>
2021-03-10 19:37:33 +0100

    Basic login example

---
+++ basic-login/README.md
@@ -0,0 +1,16 @@
+# ESP32 Discord bot basic login
+
+This is example application which represent basic login of [ESP32 Discord bot](https://github.com/abobija/esp-discord).
+
+## Configuration steps:
+
+- Run `./components/esp-discord/certgen.sh` to generate discord certificates
+- Run `idf.py set-target esp32`
+- Open `idf.py menuconfig` and set next configs:
+  - `Example Connection Configuration`
+    - \> `WiFi SSID` with your wifi name
+    - \> `WiFi Password` with you wifi password
+  - `Component Config` > `Discord` > `Token` with your bot authentication token
+- Now build, flash and monitor app.
+
+For more examples please visit [esp-discord-examples](https://github.com/abobija/esp-discord-examples) repository or check out official [esp-discord](https://github.com/abobija/esp-discord) component repository.
---
+++ basic-login/main/main.c
@@ -0,0 +1,49 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+
+#include "discord.h"
+#include "discord/session.h"
+
+static const char* TAG = "discord_bot";
+
+static discord_handle_t bot;
+
+static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t event_id, void* event_data) {
+    discord_event_data_t* data = (discord_event_data_t*) event_data;
+
+    switch(event_id) {
+        case DISCORD_EVENT_CONNECTED: {
+                discord_session_t* session = (discord_session_t*) data->ptr;
+
+                ESP_LOGI(TAG, "Bot %s#%s connected",
+                    session->user->username,
+                    session->user->discriminator
+                );
+            }
+            break;
+        
+        case DISCORD_EVENT_DISCONNECTED:
+            ESP_LOGW(TAG, "Bot logged out");
+            break;
+    }
+}
+
+void app_main(void) {
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
+
+    discord_config_t cfg = {
+        .intents = DISCORD_INTENT_GUILD_MESSAGES | DISCORD_INTENT_GUILD_MESSAGE_REACTIONS
+    };
+
+    bot = discord_create(&cfg);
+    ESP_ERROR_CHECK(discord_register_events(bot, DISCORD_EVENT_ANY, bot_event_handler, NULL));
+    ESP_ERROR_CHECK(discord_login(bot));
+}

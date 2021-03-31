9f0a7e7c1351c0f555b1f8d22e98d48e525d5058
abobija <abobijax@gmail.com>
2021-03-31 21:27:02 +0200

    Ignore msgs from bots

--- ota-multiple/main/main.c
+++ ota-multiple/main/main.c
@@ -30,7 +30,9 @@ static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t 
         
         case DISCORD_EVENT_MESSAGE_RECEIVED: {
                 discord_message_t* msg = (discord_message_t*) data->ptr;
-                discord_message_dump_log(ESP_LOGI, TAG, msg);
+                if(!msg->author->bot) {
+                    discord_message_dump_log(ESP_LOGI, TAG, msg);
+                }
                 discord_ota(bot, msg, NULL);
             }
             break;
